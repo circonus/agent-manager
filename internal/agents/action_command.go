54ba867cd5bb6baf95acd1c894f7a9364ed05c21
maier <maier@users.noreply.github.com>
2023-08-17 10:29:29 -0400

    chore: refactor command running to simplify

--- internal/agents/action_command.go
+++ internal/agents/action_command.go
@@ -33,43 +33,39 @@ func runCommands(ctx context.Context, action Action) error {
 		switch command.Command {
 		case INVENTORY:
 			if err := FetchAgents(ctx); err != nil {
-				log.Error().Err(err).Msg("refreshing agents")
+				log.Error().Err(err).Msg("refreshing agent list")
 			} else if err := CheckForAgents(ctx); err != nil {
-				log.Error().Err(err).Msg("checking for agents")
+				log.Error().Err(err).Msg("checking for installed agents")
 			}
 		case START:
 			a, ok := agents[runtime.GOOS][command.Agent]
 			if ok {
-				cmdStart(ctx, a, command)
+				runCommand(ctx, a.Start, command.ID)
 			}
 		case STOP:
 			a, ok := agents[runtime.GOOS][command.Agent]
 			if ok {
-				cmdStop(ctx, a, command)
+				runCommand(ctx, a.Stop, command.ID)
 			}
 		case RESTART:
 			a, ok := agents[runtime.GOOS][command.Agent]
 			if ok {
-				cmdRestart(ctx, a, command)
+				runCommand(ctx, a.Restart, command.ID)
 			}
 		case RELOAD:
-			// this needs to be handled differently as reload may be:
-			// a command or some type of endpoint
-			//
-			// a, ok := agents[runtime.GOOS][command.Agent]
-			// if ok {
-			// args := strings.Split(a.Reload, " ")
-			// cmd := exec.Command(args[0], args[1:]...)
-			// }
+			a, ok := agents[runtime.GOOS][command.Agent]
+			if ok {
+				cmdReload(ctx, a, command)
+			}
 		case STATUS:
 			a, ok := agents[runtime.GOOS][command.Agent]
 			if ok {
-				cmdStatus(ctx, a, command)
+				runCommand(ctx, a.Status, command.ID)
 			}
 		case VERSION:
 			a, ok := agents[runtime.GOOS][command.Agent]
 			if ok {
-				cmdVersion(ctx, a, command)
+				runCommand(ctx, a.Version, command.ID)
 			}
 		}
 	}
@@ -77,112 +73,30 @@ func runCommands(ctx context.Context, action Action) error {
 	return nil
 }
 
-func cmdStart(ctx context.Context, agent Agent, command Command) {
-	output, code, err := execute(ctx, agent.Start)
-	result := CommandResult{
-		ID: command.ID,
-		CommandData: CommandData{
-			ExitCode: code,
-		},
-	}
-
+func runCommand(ctx context.Context, cmd, id string) {
+	output, code, err := execute(ctx, cmd)
 	if err != nil {
-		result.CommandData.Error = err.Error()
+		log.Warn().Err(err).Str("output", string(output)).Int("exit_code", code).Str("cmd", cmd).Msg("command failed")
 	}
 
-	if len(output) > 0 {
-		result.CommandData.Output = base64.StdEncoding.EncodeToString(output)
-	}
+	if id != "" {
+		result := CommandResult{
+			ID: id,
+			CommandData: CommandData{
+				ExitCode: code,
+			},
+		}
 
-	if err = sendCommandResult(ctx, result); err != nil {
-		log.Error().Err(err).Msg("command result")
-	}
-}
+		if err != nil {
+			result.CommandData.Error = err.Error()
+		}
 
-func cmdStop(ctx context.Context, agent Agent, command Command) {
-	output, code, err := execute(ctx, agent.Stop)
-	result := CommandResult{
-		ID: command.ID,
-		CommandData: CommandData{
-			ExitCode: code,
-		},
-	}
+		if len(output) > 0 {
+			result.CommandData.Output = base64.StdEncoding.EncodeToString(output)
+		}
 
-	if err != nil {
-		result.CommandData.Error = err.Error()
-	}
-
-	if len(output) > 0 {
-		result.CommandData.Output = base64.StdEncoding.EncodeToString(output)
-	}
-
-	if err = sendCommandResult(ctx, result); err != nil {
-		log.Error().Err(err).Msg("command result")
-	}
-}
-
-func cmdRestart(ctx context.Context, agent Agent, command Command) {
-	output, code, err := execute(ctx, agent.Restart)
-	result := CommandResult{
-		ID: command.ID,
-		CommandData: CommandData{
-			ExitCode: code,
-		},
-	}
-
-	if err != nil {
-		result.CommandData.Error = err.Error()
-	}
-
-	if len(output) > 0 {
-		result.CommandData.Output = base64.StdEncoding.EncodeToString(output)
-	}
-
-	if err = sendCommandResult(ctx, result); err != nil {
-		log.Error().Err(err).Msg("command result")
-	}
-}
-
-func cmdVersion(ctx context.Context, agent Agent, command Command) {
-	output, code, err := execute(ctx, agent.Version)
-	result := CommandResult{
-		ID: command.ID,
-		CommandData: CommandData{
-			ExitCode: code,
-		},
-	}
-
-	if err != nil {
-		result.CommandData.Error = err.Error()
-	}
-
-	if len(output) > 0 {
-		result.CommandData.Output = base64.StdEncoding.EncodeToString(output)
-	}
-
-	if err = sendCommandResult(ctx, result); err != nil {
-		log.Error().Err(err).Msg("command result")
-	}
-}
-
-func cmdStatus(ctx context.Context, agent Agent, command Command) {
-	output, code, err := execute(ctx, agent.Status)
-	result := CommandResult{
-		ID: command.ID,
-		CommandData: CommandData{
-			ExitCode: code,
-		},
-	}
-
-	if err != nil {
-		result.CommandData.Error = err.Error()
-	}
-
-	if len(output) > 0 {
-		result.CommandData.Output = base64.StdEncoding.EncodeToString(output)
-	}
-
-	if err = sendCommandResult(ctx, result); err != nil {
-		log.Error().Err(err).Msg("command result")
+		if err = sendCommandResult(ctx, result); err != nil {
+			log.Error().Err(err).Msg("command result")
+		}
 	}
 }
