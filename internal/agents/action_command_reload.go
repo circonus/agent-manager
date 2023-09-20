d0be2d9e3587f0e5308cabd647c9d1f68834bb0d
maier <maier@users.noreply.github.com>
2023-09-20 11:03:12 -0400

    wip: reload command refactor

--- internal/agents/action_command_reload.go
+++ internal/agents/action_command_reload.go
@@ -8,14 +8,20 @@ package agents
 import (
 	"context"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"regexp"
+	"runtime"
 	"strings"
 
 	"github.com/rs/zerolog/log"
 )
+
+var serviceRx = regexp.MustCompile(`^[a-zA-z][a-zA-Z0-9\.\_\-]+$`)
 
 // NOTE: cmdReload may be called in two different contexts
 //       1. as part of installing a new configuration (most common)
@@ -24,9 +30,9 @@ import (
 func cmdReload(ctx context.Context, a Agent, command Command) {
 	switch {
 	case a.Reload == "":
+		// agent is capable of actively watching configs
+		// and dynamically reloading when changes detected.
 		return
-	case strings.ToLower(a.Reload) == RESTART:
-		runCommand(ctx, a.Restart, command.ID)
 	case strings.HasPrefix(strings.ToLower(a.Reload), "http"):
 		// http|method|body|url -- e.g. for fluent-bit "http|post||http://localhost:2020/api/v2/reload"
 		// fluent-bit -- https://docs.fluentbit.io/manual/administration/hot-reload#via-http
@@ -66,7 +72,60 @@ func cmdReload(ctx context.Context, a Agent, command Command) {
 			}
 		}
 	default:
-		runCommand(ctx, a.Reload, command.ID)
+		switch runtime.GOOS {
+		case "darwin":
+			if strings.HasPrefix(strings.ToLower(a.Reload), "brew") {
+				switch runtime.GOARCH {
+				case "amd64":
+					brew := "/usr/local/bin/brew"
+					if !doesCommandExist(brew) {
+						return
+					}
+
+					log.Warn().Str("brew", brew).Msg("darwin brew reload prefix not implemented yet")
+				case "arm64":
+					brew := "/opt/homebrew/bin/brew"
+					if !doesCommandExist(brew) {
+						return
+					}
+
+					log.Warn().Str("brew", brew).Msg("darwin brew reload prefix not implemented yet")
+				default:
+					log.Error().Str("arch", runtime.GOARCH).Str("platform", runtime.GOOS).Msg("unsupported arch for platform")
+
+					return
+				}
+			}
+		case "linux":
+			if strings.HasPrefix(strings.ToLower(a.Reload), "systemd") {
+				_, serviceName, found := strings.Cut(a.Reload, "|")
+				if !found {
+					log.Error().Str("reload", a.Reload).Msg("separator not found")
+
+					return
+				}
+
+				if serviceName == "" {
+					log.Error().Str("reload", a.Reload).Msg("missing service name after separator")
+
+					return
+				}
+
+				if !serviceRx.MatchString(serviceName) {
+					log.Error().Str("reload", a.Reload).Str("rx", serviceRx.String()).Msg("invalid service name")
+
+					return
+				}
+
+				if !isServiceEnabled(ctx, serviceName) {
+					return
+				}
+
+				runCommand(ctx, "systemctl restart "+serviceName+".service", command.ID)
+			}
+		default:
+			log.Error().Str("platform", runtime.GOOS).Msg("no reload support")
+		}
 	}
 }
 
@@ -111,4 +170,29 @@ func httpReloadRequest(ctx context.Context, method, body, rawURL string) ([]byte
 	}
 
 	return b, nil
+}
+
+func isServiceEnabled(ctx context.Context, serviceName string) bool {
+	cmd := fmt.Sprintf("systemctl is-enabled %s.service", serviceName)
+
+	output, code, err := execute(ctx, cmd)
+	if err != nil {
+		log.Error().Err(err).Str("output", string(output)).Int("exit_code", code).Str("cmd", cmd).Msg("command failed")
+
+		return false
+	}
+
+	return true
+}
+
+func doesCommandExist(cmd string) bool {
+	if _, err := os.Stat(cmd); err == nil {
+		return true
+	} else if errors.Is(err, os.ErrNotExist) {
+		log.Error().Err(err).Str("cmd", cmd).Msg("command does not exist, cannot reload agent")
+
+		return false
+	}
+
+	return false
 }
